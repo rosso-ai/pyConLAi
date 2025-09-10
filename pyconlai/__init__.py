2a587d5a3627f2c37b76a589d3c7f5788f0cc28e
Masahiko Hashimoto <hashimom@geeko.jp>
2025-09-10 21:13:53 +0900

    refactor run.py, config, and optimizer: add support for LScaffold, update CIFAR10 example with new optimizer options and transforms, adjust arguments and training loop

--- pyconlai/__init__.py
+++ pyconlai/__init__.py
@@ -1,9 +1,11 @@
 from .context import ConLPoCArguments
-from .optimizer import DSgd
+from .optimizer import load_optimizer, DSgd, LScaffold
 from .datasets import FedDatasetsClassification
 
 __all__ = [
     "ConLPoCArguments",
+    "load_optimizer",
     "DSgd",
+    "LScaffold",
     "FedDatasetsClassification",
 ]
